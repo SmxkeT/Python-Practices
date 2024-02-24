6f666b427db7296d7b41ad0448b2a31fb30a4c66
SmxkeT <tddoyle159@outlook.com>
2024-02-24 19:50:48 +0000

    Small fortune teller utilising functions

--- #Fortune teller.py
+++ #Fortune teller.py
@@ -12,6 +12,6 @@ def tellfortune(spouse, children, location, job): #function with 4 arguments to 
     print(f"You will be married to {spouse}, with {children} in {location}, working as a {job}") #displaying the 4 arguments in string.
     
 
-tellfortune("Kaoutar", "3 kids", "Japan", "Programmer")     #calling functions
-tellfortune("Kat", "2 children", "Korea", "Software Dev")
-tellfortune("neko", "1 child", "UK", "owl tamer")
+tellfortune("kat", "3 kids", "Japan", "Programmer")     #calling functionsss
+tellfortune("Kato", "2 children", "Korea", "Software Dev")
+tellfortune("katto", "1 child", "UK", "owl tamer")
