09519be0283d3d9b4bba4916fa19b7b32fd0186c
SmxkeT <tddoyle159@outlook.com>
2024-02-25 14:03:30 +0000

    Small function to convert dog to human years and vice versa.

---
+++ Dog age function.py
@@ -0,0 +1,9 @@
+#dog age calc
+
+                                    #Function to take 2 arguments(Dogs age and humans age)
+def agecalc(dage, hage):
+    print(f"Your dog is {dage*7} years old in human years, and you are {hage // 7} years old in dog years!") #Converting the ages.
+
+agecalc(2, 23)
+agecalc(5, 40)
+agecalc(10, 15) #calling the function 3 times with 3 different values. 
