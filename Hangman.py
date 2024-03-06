8f6a27aa926baeda3d1e165b4016a9a41fe31e96
SmxkeT <tddoyle159@outlook.com>
2024-03-06 23:38:49 +0000

    add game intro.

--- Hangman.py
+++ Hangman.py
@@ -4,13 +4,15 @@ import random #adding libraries.
 
 words = ["stonks", "daverino", "peetah", "test", "cat", "dog", "chungus", "apple"]
 
-def welcome():
+def welcome(): #function to introduce user to the game.
     print("Welcome to hangman.")
-    time.sleep(1)
+    time.sleep(1) #wait for 1 second.
     print("A random word will be chosen and you must figure out the word letter by letter.")
     time.sleep(1)
     print("If you can't do it fast enough, you will lose, good luck!")
     print("Game Loading....")
     time.sleep(5)
 
-
+def hangman(word, letters):
+    guess = "".join([(x if word in x else "_")for x in word])
+    return guess
