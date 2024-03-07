b9ed1d3c473134db75d5b4d20a4d5ebec3faedd5
SmxkeT <tddoyle159@outlook.com>
2024-03-07 18:23:35 +0000

    comment for function

--- Hangman.py
+++ Hangman.py
@@ -13,6 +13,6 @@ def welcome(): #function to introduce user to the game.
     print("Game Loading....")
     time.sleep(5)
 
-def hangman(word, letters):
+def hangman(word, letters): #function to take 2 arguments and join and display text as _____ 
     guess = "".join([(x if word in x else "_")for x in word])
     return guess
